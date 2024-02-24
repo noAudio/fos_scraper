06124c38c20b0493f5d9e60b945fc59a4777dcc1
Samuel Odongo <samieodd@gmail.com>
2024-02-24 12:25:21 +0300

    Create home view

---
+++ lib/home/home.dart
@@ -0,0 +1,26 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fos_scraper/common/screen.dart';
+import 'package:fos_scraper/home/components/results.dart';
+import 'package:fos_scraper/home/components/search_options.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> results = [];
+    return const Screen(
+      heading: 'Home',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SearchOptions(),
+          Column(
+            children: [Results()],
+          ),
+        ],
+      ),
+    );
+  }
+}
