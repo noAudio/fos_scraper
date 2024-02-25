547b44673a054284413603273f85bf6c3c3a4d3b
Samuel Odongo <samieodd@gmail.com>
2024-02-25 15:53:23 +0300

    Create state reducer

---
+++ lib/reducers/app_reducer.dart
@@ -0,0 +1,24 @@
+import 'package:fos_scraper/actions/index.dart';
+import 'package:fos_scraper/enums/within_options_enum.dart';
+import 'package:fos_scraper/models/app_state.dart';
+
+AppState appReducer(AppState state, dynamic action) {
+  if (action is SetKeyWordAction) {
+    return state.copyWith(keyWord: action.keyWord);
+  } else if (action is SetBusinessNameAction) {
+    return state.copyWith(businessName: action.businessName);
+  } else if (action is SetWithinOptionsAction) {
+    return state.copyWith(withinOptions: action.withinOptions);
+  } else if (action is SetDateRangeAction) {
+    return state.copyWith(startDate: action.startDate, endDate: action.endDate);
+  } else if (action is SetDecisionsAction) {
+    return state.copyWith(decisions: action.decisions);
+  } else if (action is SetSearchAction) {
+    return state.copyWith(isSearching: action.isSearching);
+  } else if (action is SetInputErrorMessageAction) {
+    return state.copyWith(inputErrorMessage: action.inputErrorMessage);
+  } else if (action is SetLogicErrorMessageAction) {
+    return state.copyWith(logicErrorMessage: action.logicErrorMessage);
+  }
+  return state;
+}
