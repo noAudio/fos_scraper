8a6aa74960f8d4ef1e354ededf6a7075a74c6fd1
Samuel Odongo <samieodd@gmail.com>
2024-02-27 16:42:10 +0300

    Connect store and dispatch actions

--- lib/home/components/search_options.dart
+++ lib/home/components/search_options.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fl;
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:fos_scraper/actions/index.dart';
+import 'package:fos_scraper/enums/decisions_enum.dart';
+import 'package:fos_scraper/enums/within_options_enum.dart';
+import 'package:fos_scraper/models/app_state.dart';
 import 'package:fos_scraper/scrape_status/scrape_status.dart';
 
 class SearchOptions extends StatefulWidget {
@@ -15,96 +20,139 @@ class _SearchOptionsState extends State<SearchOptions> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child: Row(
-            children: [
-              fl.InfoLabel(
-                label: 'Keyword or Product',
-                child: const SizedBox(
-                  width: 250,
-                  child: fl.TextBox(
-                    autofocus: true,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 25,
-              ),
-              fl.InfoLabel(
-                label: 'Business name',
-                child: const SizedBox(
-                  width: 250,
-                  child: fl.TextBox(),
-                ),
-              ),
-            ],
-          ),
-        ),
-        // within
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child: Column(
+    return StoreConnector<AppState, dynamic>(
+        converter: (store) => store,
+        builder: (context, store) {
+          var state = store.state as AppState;
+          return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              fl.InfoLabel(label: 'Within'),
-              const Option(label: 'Banking, credit and mortgages'),
-              const Option(label: 'Investment and pensions'),
-              const Option(label: 'Insurance (excluding PPI)'),
-              const Option(label: 'Payment protection insurance (PPI)'),
-              const Option(label: 'Claims Management Ombudsman decisions'),
-              const Option(label: 'Funeral plans'),
-            ],
-          ),
-        ),
-        // date range
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child: Row(
-            children: [
-              fl.DatePicker(
-                header: 'Date from',
-                selected: startDate,
-                onChanged: (time) => setState(() => startDate = time),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    fl.InfoLabel(
+                      label: 'Keyword or Product',
+                      child: const SizedBox(
+                        width: 250,
+                        child: fl.TextBox(
+                          autofocus: true,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 25,
+                    ),
+                    fl.InfoLabel(
+                      label: 'Business name',
+                      child: const SizedBox(
+                        width: 250,
+                        child: fl.TextBox(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                width: 25,
+              // within
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    fl.InfoLabel(label: 'Within'),
+                    const Option(
+                      label: 'Banking, credit and mortgages',
+                      type: WithinOptionsEnum.bankingCreditMortgages,
+                    ),
+                    const Option(
+                      label: 'Investment and pensions',
+                      type: WithinOptionsEnum.investmentPensions,
+                    ),
+                    const Option(
+                      label: 'Insurance (excluding PPI)',
+                      type: WithinOptionsEnum.insuranceExcludingPPI,
+                    ),
+                    const Option(
+                      label: 'Payment protection insurance (PPI)',
+                      type: WithinOptionsEnum.paymentProtectionInsurance,
+                    ),
+                    const Option(
+                      label: 'Claims Management Ombudsman decisions',
+                      type:
+                          WithinOptionsEnum.claimsManagementOmbudsmanDecisions,
+                    ),
+                    const Option(
+                      label: 'Funeral plans',
+                      type: WithinOptionsEnum.funeralPlans,
+                    ),
+                  ],
+                ),
               ),
-              fl.DatePicker(
-                header: 'Date to',
-                selected: endDate,
-                onChanged: (time) => setState(() => endDate = time),
+              // date range
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    fl.DatePicker(
+                      header: 'Date from',
+                      selected: state.startDate,
+                      onChanged: (time) {
+                        store.dispatch(SetStartDateAction(startDate: time));
+                      },
+                    ),
+                    const SizedBox(
+                      width: 25,
+                    ),
+                    fl.DatePicker(
+                      header: 'Date to',
+                      selected: state.endDate,
+                      onChanged: (time) {
+                        store.dispatch(SetEndDateAction(endDate: time));
+                      },
+                    ),
+                  ],
+                ),
               ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    fl.InfoLabel(label: 'Decisions that were'),
+                    const Option(
+                      label: 'Upheld',
+                      type: DecisionsEnum.upheld,
+                    ),
+                    const Option(
+                      label: 'Not upheld',
+                      type: DecisionsEnum.notUpheld,
+                    ),
+                  ],
+                ),
+              ),
+              fl.FilledButton(
+                onPressed: state.isSearching
+                    ? null
+                    : () {
+                        store.dispatch(SetSearchAction(isSearching: true));
+                      },
+                child: fl.Text(state.isSearching ? 'Searching...' : 'Search'),
+              ),
+              state.isSearching ? const ScrapeStatus() : const SizedBox(),
             ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              fl.InfoLabel(label: 'Decisions that were'),
-              const Option(label: 'Upheld'),
-              const Option(label: 'Not upheld'),
-            ],
-          ),
-        ),
-        fl.FilledButton(
-          child: const fl.Text('Search'),
-          onPressed: () => setState(() => print('Searching!')),
-        ),
-        const ScrapeStatus(),
-      ],
-    );
+          );
+        });
   }
 }
 
 class Option extends StatefulWidget {
   final String label;
-  const Option({super.key, required this.label});
+  final dynamic type;
+  const Option({
+    super.key,
+    required this.label,
+    required this.type,
+  });
 
   @override
   State<Option> createState() => _OptionState();
@@ -114,13 +162,38 @@ class _OptionState extends State<Option> {
   bool? isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return fl.InfoLabel(
-      label: widget.label,
-      isHeader: false,
-      child: fl.Checkbox(
-        checked: isChecked,
-        onChanged: (value) => setState(() => isChecked = value),
-      ),
-    );
+    return StoreConnector<AppState, dynamic>(
+        converter: (store) => store,
+        builder: (context, store) {
+          void onClick() {
+            if (isChecked!) {
+              if (widget.type is WithinOptionsEnum) {
+                store.dispatch(
+                    AddWithinOptionsAction(withinOption: widget.type));
+              } else {
+                store.dispatch(AddDecisionsAction(decision: widget.type));
+              }
+            } else {
+              if (widget.type is WithinOptionsEnum) {
+                store.dispatch(
+                    RemoveWithinOptionsAction(withinOption: widget.type));
+              } else {
+                store.dispatch(RemoveDecisionsAction(decision: widget.type));
+              }
+            }
+          }
+
+          return fl.InfoLabel(
+            label: widget.label,
+            isHeader: false,
+            child: fl.Checkbox(
+              checked: isChecked,
+              onChanged: (value) {
+                setState(() => isChecked = value);
+                onClick();
+              },
+            ),
+          );
+        });
   }
 }
