481ec3c288684765840dc762a8e4577a8202e10a
Samuel Odongo <samieodd@gmail.com>
2024-02-27 15:02:17 +0300

    Add action to remove within option

---
+++ lib/actions/remove_within_options_action.dart
@@ -0,0 +1,9 @@
+import 'package:fos_scraper/enums/within_options_enum.dart';
+
+class RemoveWithinOptionsAction {
+  final WithinOptionsEnum withinOption;
+
+  RemoveWithinOptionsAction({
+    required this.withinOption,
+  });
+}
