cc2f866f8a28f65eea400fc7086630bcc2421290
Samuel Odongo <samieodd@gmail.com>
2024-02-28 12:36:20 +0300

    Add method to build link

--- lib/home/components/search_button.dart
+++ lib/home/components/search_button.dart
@@ -2,6 +2,8 @@ import 'package:fluent_ui/fluent_ui.dart' as fl;
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:fos_scraper/actions/set_search_action.dart';
+import 'package:fos_scraper/enums/decisions_enum.dart';
+import 'package:fos_scraper/enums/within_options_enum.dart';
 import 'package:fos_scraper/models/app_state.dart';
 
 class SearchButton extends StatelessWidget {
@@ -10,7 +12,54 @@ class SearchButton extends StatelessWidget {
   String linkBuilder(AppState state) {
     // TODO: Build link
     // example link: https://www.financial-ombudsman.org.uk/decisions-case-studies/ombudsman-decisions/search?Keyword=estate&BusinessName=house&Business=house&IndustrySectorID%5B1%5D=1&IndustrySectorID%5B2%5D=2&IndustrySectorID%5B3%5D=3&IndustrySectorID%5B4%5D=4&IndustrySectorID%5B5%5D=5&IndustrySectorID%5B6%5D=6&DateFrom=2020-02-27&DateTo=2024-02-27&IsUpheld%5B1%5D=1&IsUpheld%5B0%5D=0&Sort=relevance
-    return '';
+    String keyWord = state.keyWord == '' ? '' : 'Keyword=${state.keyWord}&';
+    String businessName = state.businessName == ''
+        ? ''
+        : 'BusinessName=${state.businessName}&Business=${state.businessName}&';
+    String withinOptions = '';
+    if (state.withinOptions.isNotEmpty) {
+      for (var option in state.withinOptions) {
+        int value = option == WithinOptionsEnum.bankingCreditMortgages
+            ? 1
+            : option == WithinOptionsEnum.investmentPensions
+                ? 2
+                : option == WithinOptionsEnum.insuranceExcludingPPI
+                    ? 3
+                    : option == WithinOptionsEnum.paymentProtectionInsurance
+                        ? 4
+                        : option ==
+                                WithinOptionsEnum
+                                    .claimsManagementOmbudsmanDecisions
+                            ? 5
+                            : option == WithinOptionsEnum.funeralPlans
+                                ? 6
+                                : 0;
+        if (value != 0) {
+          withinOptions =
+              '${withinOptions}IndustrySectorID%5B$value%5D=$value&';
+        }
+      }
+    }
+    // TODO: Format dates properly
+    // TODO: Check how giving only a start date changes the link
+    String startDate =
+        state.startDate != null ? 'DateFrom=${state.startDate}&' : '';
+    String endDate = state.endDate != null ? 'DateTo=${state.endDate}&' : '';
+    String decisions = '';
+    if (state.decisions.isNotEmpty) {
+      for (var decision in state.decisions) {
+        var value = decision == DecisionsEnum.upheld
+            ? 0
+            : decision == DecisionsEnum.notUpheld
+                ? 1
+                : -1;
+        if (value != -1) {
+          decisions = '${decisions}IsUpheld%5B$value%5D=$value&';
+        }
+      }
+    }
+    // TODO: Sort by date
+    return 'https://www.financial-ombudsman.org.uk/decisions-case-studies/ombudsman-decisions/search?$keyWord$businessName$withinOptions$startDate$endDate${decisions}Sort=relevance';
   }
 
   @override
