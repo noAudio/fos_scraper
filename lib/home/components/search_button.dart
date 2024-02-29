086efef6ae5798ea4dd3242228d7f935cfa546c5
Samuel Odongo <samieodd@gmail.com>
2024-02-29 20:21:23 +0300

    Fix imports

--- lib/home/components/search_button.dart
+++ lib/home/components/search_button.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:fluent_ui/fluent_ui.dart' as fl;
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:fos_scraper/actions/index.dart';
-import 'package:fos_scraper/actions/set_search_action.dart';
 import 'package:fos_scraper/enums/decisions_enum.dart';
 import 'package:fos_scraper/enums/within_options_enum.dart';
 import 'package:fos_scraper/logic/scraper/scraper.dart';
